3fe3e8207e138b6db7860011f84c3391643ba84a
antsuarz <78346323+antsuarz@users.noreply.github.com>
2021-03-26 11:07:59 +0100

    SDI-2021-312-7.2-Express, depuración y variables de entorno.

---
+++ app.js
@@ -0,0 +1,18 @@
+// Módulos
+let express = require('express');
+let app = express();
+
+app.set('port', 8081);
+
+app.get('/usuarios', function (req, res){
+    console.log("depurar aquí");
+    res.send('ver usuarios');
+});
+
+app.get('/canciones', function (req, res){
+    res.send('ver cacniones');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Servidor activo');
+});
