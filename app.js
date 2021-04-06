9d02c9d58695a4738799ec658e5176f335d22c5a
antsuarz <78346323+antsuarz@users.noreply.github.com>
2021-04-06 18:39:43 +0200

    SDI–2021-312-8.3-Subida a ficheros y gestión de colecciones.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 let express = require('express');
 let app = express();
 
+let fileUpload = require('express-fileupload');
+app.use(fileUpload());
+
 let bodyParser = require('body-parser');
 
 let mongo = require('mongodb');
