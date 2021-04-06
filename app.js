40cd5e648262b8ed4e13a3c5f53d0bfba3501c31
antsuarz <78346323+antsuarz@users.noreply.github.com>
2021-04-06 19:20:52 +0200

    SDI-2021-312-8.5-Colecciones relacionadas y routers.

--- app.js
+++ app.js
@@ -13,11 +13,8 @@ let crypto = require('crypto');
 
 let fileUpload = require('express-fileupload');
 app.use(fileUpload());
-
 let bodyParser = require('body-parser');
-
 let mongo = require('mongodb');
-
 let swig = require('swig');
 
 let gestorBD = require("./modules/gestorBD.js");
@@ -26,7 +23,6 @@ gestorBD.init(app,mongo);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
 
 app.set('port', 8081);
 
@@ -34,10 +30,47 @@ app.set('db','mongodb://admin:sdi@tiendamusica-shard-00-00.jxgw2.mongodb.net:270
 app.set('clave','abcdefg');
 app.set('crypto',crypto);
 
-require("./routes/rusuarios.js")(app,swig, gestorBD);
-require("./routes/rcanciones.js")(app, swig, gestorBD);
-require("./routes/rautores.js")(app, swig);
 
 app.listen(app.get('port'), function(){
     console.log('Servidor activo');
 });
+
+var routerUsuarioSession = express.Router();
+routerUsuarioSession.use(function(req, res, next) {
+    console.log("routerUsuarioSession");
+    if ( req.session.usuario ) {
+        next();
+    } else {
+        console.log("va a : "+req.session.destino)
+        res.redirect("/identificarse");
+    }
+});
+
+//routerAudios
+let routerAudios = express.Router();
+routerAudios.use(function(req, res, next) {
+    console.log("routerAudios");
+    let path = require('path');
+    let idCancion = path.basename(req.originalUrl, '.mp3');
+    gestorBD.obtenerCanciones(
+        {"_id": mongo.ObjectID(idCancion) }, function (canciones) {
+            if(req.session.usuario && canciones[0].autor == req.session.usuario ){
+                next();
+            } else {
+                res.redirect("/tienda");
+            }
+        })
+});
+
+//Aplicar routerAudios
+app.use("/audios/",routerAudios);
+//Aplicar routerUsuarioSession
+app.use("/canciones/agregar",routerUsuarioSession);
+app.use("/publicaciones",routerUsuarioSession);
+
+
+require("./routes/rusuarios.js")(app,swig, gestorBD);
+require("./routes/rcanciones.js")(app, swig, gestorBD);
+require("./routes/rautores.js")(app, swig);
+
+app.use(express.static('public'));
