27c5bbc171b79eb5aded785425e7644af31cd05e
antsuarz <78346323+antsuarz@users.noreply.github.com>
2021-04-15 23:42:39 +0200

    SDI-2021-312-9.1-Gestión de colecciones, eliminar documento.

--- app.js
+++ app.js
@@ -68,6 +68,28 @@ app.use("/audios/",routerAudios);
 app.use("/canciones/agregar",routerUsuarioSession);
 app.use("/publicaciones",routerUsuarioSession);
 
+//routerUsuarioAutor
+let routerUsuarioAutor = express.Router();
+routerUsuarioAutor.use(function(req, res, next) {
+    console.log("routerUsuarioAutor");
+    let path = require('path');
+    let id = path.basename(req.originalUrl);
+// Cuidado porque req.params no funciona
+// en el router si los params van en la URL.
+    gestorBD.obtenerCanciones(
+        {_id: mongo.ObjectID(id) }, function (canciones) {
+            console.log(canciones[0]);
+            if(canciones[0].autor == req.session.usuario ){
+                next();
+            } else {
+                res.redirect("/tienda");
+            }
+        })
+});
+//Aplicar routerUsuarioAutor
+app.use("/cancion/modificar",routerUsuarioAutor);
+app.use("/cancion/eliminar",routerUsuarioAutor);
+
 
 require("./routes/rusuarios.js")(app,swig, gestorBD);
 require("./routes/rcanciones.js")(app, swig, gestorBD);
