d21c789bd6b637c54e699f1372b40e30c2e5769d
antsuarz <78346323+antsuarz@users.noreply.github.com>
2021-04-20 20:20:22 +0200

    SDI-2021-312-10.2-SW API REST Autenticación.

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ let app = express();
 
 let fs = require('fs');
 let https = require('https');
+let jwt = require('jsonwebtoken');
+app.set('jwt',jwt);
+
 
 
 let expressSession = require('express-session');
@@ -64,6 +67,42 @@ routerUsuarioSession.use(function(req, res, next) {
         res.redirect("/identificarse");
     }
 });
+
+// routerUsuarioToken
+let routerUsuarioToken = express.Router();
+routerUsuarioToken.use(function(req, res, next) {
+    // obtener el token, vía headers (opcionalmente GET y/o POST).
+    let token = req.headers['token'] || req.body.token || req.query.token;
+    if (token != null) {
+        // verificar el token
+        jwt.verify(token, 'secreto', function(err, infoToken) {
+            if (err || (Date.now()/1000 - infoToken.tiempo) > 240 ){
+                res.status(403); // Forbidden
+                res.json({
+                    acceso : false,
+                    error: 'Token invalido o caducado'
+                });
+                // También podríamos comprobar que intoToken.usuario existe
+                return;
+
+            } else {
+                // dejamos correr la petición
+                res.usuario = infoToken.usuario;
+                next();
+            }
+        });
+
+    } else {
+        res.status(403); // Forbidden
+        res.json({
+            acceso : false,
+            mensaje: 'No hay Token'
+        });
+    }
+});
+
+// Aplicar routerUsuarioToken
+app.use('/api/cancion', routerUsuarioToken);
 
 //routerAudios
 let routerAudios = express.Router();
